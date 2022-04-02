89f2123fa019268d0315b2a54d34a2208371d87c
Naol Abera <naolcovered@gmail.com>
2022-04-02 15:16:57 +0300

    Timer flutter app

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:timer_flutter_project/app.dart';
+
+void main() => runApp(App());
---
+++ lib/view/timer_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:timer_flutter_project/bloc/timer_bloc.dart';
+import 'package:timer_flutter_project/bloc/timer_state.dart';
+import 'package:timer_flutter_project/ticker.dart';
+import 'package:timer_flutter_project/view/background.dart';
+
+import '../bloc/timer_event.dart';
+
+class TimerPage extends StatelessWidget {
+  const TimerPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => TimerBloc(ticker: Ticker()),
+      child: const TimerView(),
+    );
+  }
+}
+
+class TimerView extends StatelessWidget {
+  const TimerView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Timer')),
+      body: Stack(
+        children: [
+          const Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100.0),
+                child: Center(child: TimerText()),
+              ),
+              Action(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TimerText extends StatelessWidget {
+  const TimerText({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final duration = context.select((TimerBloc bloc) => bloc.state.duration);
+    final minutesStr =
+        ((duration / 60) % 60).floor().toString().padLeft(2, '0');
+    final secondsStr = (duration % 60).floor().toString().padLeft(2, '0');
+    return Text(
+      '$minutesStr:$secondsStr',
+      style: Theme.of(context).textTheme.headline1,
+    );
+  }
+}
+
+
+class Action extends StatelessWidget {
+  const Action({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TimerBloc, TimerState>(
+      buildWhen: (prev, state) => prev.runtimeType != state.runtimeType,
+      builder: (context, state) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            if (state is TimerInitial) ...[
+              FloatingActionButton(
+                child: Icon(Icons.play_arrow),
+                onPressed: () => context
+                    .read<TimerBloc>()
+                    .add(TimerStarted(duration: state.duration)),
+              ),
+            ],
+            if (state is TimerRunInProgress) ...[
+              FloatingActionButton(
+                child: Icon(Icons.pause),
+                onPressed: () => context.read<TimerBloc>().add(TimerPaused()),
+              ),
+              FloatingActionButton(
+                child: Icon(Icons.replay),
+                onPressed: () => context.read<TimerBloc>().add(TimerReset()),
+              ),
+            ],
+            if (state is TimerRunPause) ...[
+              FloatingActionButton(
+                child: Icon(Icons.play_arrow),
+                onPressed: () => context.read<TimerBloc>().add(TimerResumed()),
+              ),
+              FloatingActionButton(
+                child: Icon(Icons.replay),
+                onPressed: () => context.read<TimerBloc>().add(TimerReset()),
+              ),
+            ],
+            if (state is TimerRunComplete) ...[
+              FloatingActionButton(
+                child: Icon(Icons.replay),
+                onPressed: () => context.read<TimerBloc>().add(TimerReset()),
+              ),
+            ]
+          ],
+        );
+      },
+    );
+  }
+}
